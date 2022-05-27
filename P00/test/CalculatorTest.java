e0926bba65556f107ee4a858ffd00f9738627896
21020419 <21020419@LAPTOP-IPRV933E>
2022-05-27 11:55:45 +0800

    first push to P00 respository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,94 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals (expected,actual);
+	}
+	
+	@Test
+	public void testSubtract() {
+		//fail("Not yet implemented");
+		int a = 5000;
+		int b = 1000;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a,b);
+		
+		int expected = 4000;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testMulitple() {
+		//fail("Not yet implemented");
+		int a = 2000;
+		int b = 2;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiple(a,b);
+		
+		int expected = 4000;
+		assertEquals(expected, actual);
+	}
+	
+	@Test 
+	public void testDivide() {
+		int a = 1000;
+		int b = 2;
+		
+//		if(b == 0) {
+//			String output = ("Error. Cannot have 0 denominator");
+//		}
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		
+		int expected = 500;
+		assertEquals(expected, actual);
+	}
+	
+	
+		
+}
+
+
+
+
+
+
+
+
+
+
